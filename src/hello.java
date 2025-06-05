51a2f2edf70a8deeb937a460691dbb8629a8d0a1
Basheermisbah <basheeruddinm023@gmail.com>
2025-06-05 23:20:54 +0530

    Hello class updated

--- src/hello.java
+++ src/hello.java
@@ -2,5 +2,10 @@ public class hello
 {
     public static void main(String[] args) {
         System.out.println("Github session");
+
+        System.out.println("Github session");
+        System.out.println("Github session");
+        System.out.println("Github session");
+
     }
 }
